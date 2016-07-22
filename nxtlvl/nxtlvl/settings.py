914ee4e4b279e51e518a16c97554afd0eef120dc
Andrey Pismennyj <apismennyj@gmail.com>
2016-07-22 13:17:04 +0300

    DATABASE_URL introduced

--- nxtlvl/nxtlvl/settings.py
+++ nxtlvl/nxtlvl/settings.py
@@ -11,10 +11,24 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url as dburl
+
+
+def environ(key):
+    """
+    returns True if the key exists in os.environ[] and it is not false'ish
+    """
+
+    if not key in os.environ.keys():
+        return False
+    if os.environ[key] and os.environ[key] != '0':
+        return True
+
+    return False
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -73,14 +87,42 @@ WSGI_APPLICATION = 'nxtlvl.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+#
+# MYSQL steps to create database and user:
+#
+# CREATE DATABASE nxtlvl_db CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE USER 'nxtlvl_user' IDENTIFIED BY '1q2w3e4r';
+# GRANT ALL ON nxtlvl_db.* TO nxtlvl_user@localhost IDENTIFIED BY '';
+# GRANT ALL ON nxtlvl_db.* TO 'nxtlvl_user'@'localhost';
+#
+# Define DATABASE_URL in your local environment by running:
+#
+# export DATABASE_URL="mysql://nxtlvl_user:1q2w3e4r@localhost:16903/nxtlvl_db"
+#
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nxtlvl_db',
+        'USER': 'nxtlvl_user',
+        'PASSWORD': '1q2w3e4r',
+
+    },
+    'reference': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nxtlvl_test',
+        'USER': 'nxtlvl_user',
+        'PASSWORD': '1q2w3e4r',
+
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+# Overwriting `default` from environment's DATABASE_URL
+if environ('DATABASE_URL'):
+    DATABASES['default'] = dburl.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
