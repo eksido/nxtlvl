714102777674a31d30fc0bcfbf062ee756ef29bb
Andrey Pismennyj <apismennyj@gmail.com>
2016-07-25 11:59:38 +0300

    urls configured

--- nxtlvl/ensomus/urls.py
+++ nxtlvl/ensomus/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth import views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from .views import notimplemented
 
 admin.autodiscover()
 urlpatterns = patterns('',
-                url(r'^admin/', include(admin.site.urls)),
+                url('', notimplemented, name='index'),
+                url(r'^user$', notimplemented, name='index'),
 )
