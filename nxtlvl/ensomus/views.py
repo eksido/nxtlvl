a5b63de7136d810623eb62f794d2e2830bbdac3e
Andrey Pismennyj <apismennyj@gmail.com>
2016-07-25 14:07:52 +0300

    static config added

--- nxtlvl/ensomus/views.py
+++ nxtlvl/ensomus/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'base.html', context)
+    return render(request, 'index.html', context)
 
 def notimplemented(request):
     return HttpResponse("NXTLVL Method not implemented yet", status=501)
--- nxtlvl/nxtlvl/settings/default.py
+++ nxtlvl/nxtlvl/settings/default.py
@@ -83,7 +83,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates')
+            os.path.join(PROJECT_DIR, 'templates'),
+            STATIC_ROOT
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -172,6 +173,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # TinyMCE
 
